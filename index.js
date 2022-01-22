a2d18dc1d039591f85f1a76b5beee90bb90dbdd2
David Chou <davidchou2009@gmail.com>
2022-01-22 12:25:22 -0800

    create function to write to readme

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const genMarkdown = require('./generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    
+}
 // Function call to initialize app
 init();
