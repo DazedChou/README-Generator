1fa2ec3a9726c7ce490157d4b444849649aade77
David Chou <davidchou2009@gmail.com>
2022-01-22 12:41:54 -0800

    create array of questions for user input

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const genMarkdown = require('./generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?'];
+const questions = ['What is the title of your project?',
+'Give a description of your project.',
+'What are the steps required to install your project?',
+'Provide instructions and examples for use.',
+'Choose a license', 'Would you like other developers to contribute to your project?',
+'Go the extra mile and write tests for your application. Then provide examples on how to run them.'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
