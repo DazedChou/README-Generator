2c416370f57b5ceb542e1458a0876e33f113f664
David Chou <davidchou2009@gmail.com>
2022-01-22 19:34:39 -0800

    create function to return license link and badge

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license){
+    case "Apache License 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU General Public License v3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Boost Software License 1.0":
+      return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    case "Creative Commons Zero v1.0 Universal":
+      return "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    case "Eclipse Public License 2.0":
+      return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+    default:
+      return "";
+    }
+    
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license){
+    case "Apache License 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GNU General Public License v3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "MIT License":
+      return "https://opensource.org/licenses/MIT";
+    case "Boost Software License 1.0":
+      return "https://www.boost.org/LICENSE_1_0.txt";
+    case "Creative Commons Zero v1.0 Universal":
+      return "(http://creativecommons.org/publicdomain/zero/1.0/";
+    case "Eclipse Public License 2.0":
+      return "https://opensource.org/licenses/EPL-1.0"
+    default:
+      return "";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -50,4 +85,4 @@ function generateMarkdown(data) {
   ${response.profile}`;;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown()};
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const questions = ['What is the title of your project?',
 'Give a description of your project.',
 'What are the steps required to install your project?',
 'Provide instructions and examples for use.',
-'Choose a license', 'Would you like other developers to contribute to your project?',
-'Go the extra mile and write tests for your application. Then provide examples on how to run them.'];
+'Choose a license', 'Would you like to add a contributor covenant?',
+'Go the extra mile and write tests for your application. Then provide examples on how to run them.',
+'Enter in your email address for contact','Enter in your Github profile URL'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -26,6 +27,41 @@ function init() {
                 message: questions[0],
                 name: 'title',
             },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage',
+            },
+            {
+                type: 'list',
+                message: questions[4],
+                name: 'licenses',
+                choices: ['Apache License 2.0','GNU General Public License v3.0','MIT License',
+                'Boost Software License 1.0','Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 2.0'],
+            },
+            {
+                type: 'list',
+                message: questions[5],
+                name: 'contribution',
+                choices: ['yes','no'],
+            },
+            {
+                type: 'input',
+                message: questions[6],
+                name: 'test',
+            },
+
         ])
         .then((response) => {
             const md = genMarkdown.generateMarkdown(response);
